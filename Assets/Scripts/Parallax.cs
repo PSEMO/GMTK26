3bd9f3a647541f29d7764823f51433f7cf69a460
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-07-24 21:51:19 +0300

    parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] List<GameObject> backgrounds;
+    [SerializeField] private float[] parallaxScales;
+    [SerializeField] private float smoothing = 1f;
+    
+    private Vector3 previousCamPos;
+
+    private void Start()
+    {
+        previousCamPos = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < backgrounds.Count; i++)
+        {
+            if (backgrounds[i] == null) continue;
+
+            float parallaxX = (previousCamPos.x - transform.position.x) * parallaxScales[i];
+            float parallaxY = (previousCamPos.y - transform.position.y) * parallaxScales[i];
+
+            Vector3 targetPos = new Vector3(
+                backgrounds[i].transform.position.x + parallaxX,
+                backgrounds[i].transform.position.y + parallaxY,
+                backgrounds[i].transform.position.z
+            );
+
+            backgrounds[i].transform.position = Vector3.Lerp(
+                backgrounds[i].transform.position,
+                targetPos,
+                smoothing * Time.deltaTime
+            );
+        }
+
+        previousCamPos = transform.position;
+    }
+}
