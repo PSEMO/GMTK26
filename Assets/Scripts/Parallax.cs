ce64056d3ec05fb295e6f7dd6e02258d498382e3
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-07-26 15:58:23 +0300

    infinite parallax

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,41 +1,74 @@
 using System.Collections.Generic;
+using PSEMO.Camera;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] List<GameObject> backgrounds;
+    [SerializeField] CameraSO CamData;
+
+    [SerializeField] List<GameObject> backgroundsLeft;
+    [SerializeField] List<GameObject> backgroundsMiddle;
+    [SerializeField] List<GameObject> backgroundsRight;
     [SerializeField] private Vector3[] parallaxScales;
     [SerializeField] private float smoothing = 1f;
     
     private Vector3 previousCamPos;
+    private float[] widths;
 
-    private void Start()
+    private void Awake()
     {
-        previousCamPos = transform.position;
+        previousCamPos = CamData.positionOffset;
+        InitializeWidths();
+    }
+
+    private void InitializeWidths()
+    {
+        int count = backgroundsMiddle.Count;
+        widths = new float[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            widths[i] = Mathf.Abs(backgroundsRight[i].transform.position.x - backgroundsMiddle[i].transform.position.x);
+        }
     }
 
     private void LateUpdate()
     {
-        for (int i = 0; i < backgrounds.Count; i++)
-        {
-            if (backgrounds[i] == null) continue;
+        int count = backgroundsMiddle.Count;
 
+        for (int i = 0; i < count; i++)
+        {
             float parallaxX = (previousCamPos.x - transform.position.x) * parallaxScales[i].x;
             float parallaxY = (previousCamPos.y - transform.position.y) * parallaxScales[i].y;
 
-            Vector3 targetPos = new Vector3(
-                backgrounds[i].transform.position.x + parallaxX,
-                backgrounds[i].transform.position.y + parallaxY,
-                backgrounds[i].transform.position.z
-            );
+            MoveBackground(backgroundsLeft[i], parallaxX, parallaxY);
+            MoveBackground(backgroundsMiddle[i], parallaxX, parallaxY);
+            MoveBackground(backgroundsRight[i], parallaxX, parallaxY);
 
-            backgrounds[i].transform.position = Vector3.Lerp(
-                backgrounds[i].transform.position,
-                targetPos,
-                smoothing * Time.deltaTime
-            );
+            if (backgroundsRight[i].transform.position.x <= transform.position.x)
+            {
+                MoveBackground(backgroundsLeft[i], widths[i], 0);
+                MoveBackground(backgroundsMiddle[i], widths[i], 0);
+                MoveBackground(backgroundsRight[i], widths[i], 0);
+            }
+            else if (backgroundsLeft[i].transform.position.x >= transform.position.x)
+            {
+                MoveBackground(backgroundsLeft[i], -widths[i], 0);
+                MoveBackground(backgroundsMiddle[i], -widths[i], 0);
+                MoveBackground(backgroundsRight[i], -widths[i], 0);
+            }
         }
 
         previousCamPos = transform.position;
+    }
+
+    private void MoveBackground(GameObject bg, float parallaxX, float parallaxY)
+    {
+        bg.transform.position = new(
+            bg.transform.position.x + parallaxX,
+            bg.transform.position.y + parallaxY,
+            bg.transform.position.z
+        );
     }
 }
